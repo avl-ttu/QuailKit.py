8bc37afaa9b17469c20ed169b0fb136b8af6db50
Joel Reznick <32008471+bozingle@users.noreply.github.com>
2019-03-23 22:43:29 -0500

    Update JRData.py

--- JRData.py
+++ JRData.py
@@ -10,36 +10,36 @@ import numpy as np
 
 class JRData:
     def __init__(self,filepath, setting = 'spgram'):
-        #Loading in data from .mat file
         self.set = setting
         self.filepath = filepath
         self.h5File = h5py.File(self.filepath,'r')
-        self.audioDS = self.h5File['audio']
-        self.spgramDS = self.h5File['spgrams/spgram1']
-        self.sizeDS = len(self.h5File['spgrams'])
+        self.audioDS = self.h5File['c1/audio']
+        self.spgramDS = self.h5File['c1/spgram']
+        self.sizeDS = len(self.h5File['/'])
         self.spgramfs = self.spgramDS.attrs["props"][0]
         self.finalTimeSpgram = self.spgramDS.attrs["props"][1]
         self.fBegin = self.spgramDS.attrs["props"][2]
         self.fEnd = self.spgramDS.attrs["props"][3]
         self.scale = self.spgramDS.attrs["props"][4]
         self.datetime = self.h5File['/'].attrs["props"]
-        print(self.datetime,"h")
         self.audiofs = self.audioDS.attrs["audiofs"][0]
         
-    def __call__(self, setting):
-        if setting[0:6] == 'spgram' and int(setting[6]) <= self.sizeDS:
-            self.set = setting[0:6]
-            self.spgramDS = self.h5File['spgrams/'+setting]
+    def __call__(self, channel, setting):
+        if setting == 'spgram' and int(channel[1]) <= self.sizeDS:
+            self.set = setting
+            self.spgramDS = self.h5File[channel+'/'+setting]
             self.spgramfs = self.spgramDS.attrs["props"][0]
             self.finalTimeSpgram = self.spgramDS.attrs["props"][1]
             self.fBegin = self.spgramDS.attrs["props"][2]
             self.fEnd = self.spgramDS.attrs["props"][3]
             self.scale = self.spgramDS.attrs["props"][4]
-        elif setting == 'audio':
-            self.set = setting
+        elif setting == 'audio' and int(channel[1]) <= self.sizeDS:
+            self.set = setting 
+            self.audioDS = self.h5File[channel+'/audio']
         else:
             print("Can't do.")
         return self
+        
     
     def __getitem__(self, interval):
         s = []
@@ -51,7 +51,6 @@ class JRData:
             t = np.arange(interval[0],interval[1],step)
             s = self.spgramDS[:, startIn:endIn]
             step=(self.fEnd-self.fBegin)/1000
-            print(self.fBegin,self.fEnd)
             f = np.arange(self.fBegin,self.fEnd+1,step)
             return t, f, s
         elif self.set == "audio":
@@ -66,11 +65,10 @@ class JRData:
         self.h5File.close()
 
 DataObj = JRData("C:\\Users\\joggl\\Texas Tech University\\Quail Call - Joel\\QuailKit\\JR_QuailKit\\SM304472_0+1_20181219$100000.h5")
-
 # #Long way
-DataObj('spgram1')
+DataObj('c2','spgram')
 t,f,s = DataObj[0,100]
-#print(t.shape,f.shape,s.shape)
+print(t.shape,f.shape, s.shape)
 # print(1)
 # #Short hand
 # t,s = DataObj('audio')[0,10]
